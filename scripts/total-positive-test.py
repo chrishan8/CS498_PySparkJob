d8cb19cab027df2623ead75d78df571302bc3d1b
Chris Han <chris.han8@gmail.com>
2018-04-26 22:44:53 -0400

    using jars config

--- scripts/total-positive-test.py
+++ scripts/total-positive-test.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 BUCKET_NAME = "cs498mc"
 KEY_NAME = "tweets.json"
 
-conf = SparkConf().setMaster("local").setAppName("TotalPositiveTest").set("spark.driver.extraClassPath", os.path.join(os.path.dirname(__file__), '../jars/aws-java-sdk-1.7.4.jar')).set("spark.driver.extraClassPath", os.path.join(os.path.dirname(__file__), '../jars/hadoop-aws-2.7.1.jar')).set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem").set("spark.hadoop.fs.s3a.access.key", ACCESS_KEY).set("spark.hadoop.fs.s3a.secret.key", SECRET_KEY)
+conf = SparkConf().setMaster("local").setAppName("TotalPositiveTest").set("spark.jars", "{}/*jar".format(os.path.join(os.path.dirname(__file__), '../jars'))).set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
 sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
