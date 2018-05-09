dbb7ac34a8ec38203ec5336781f1b7faef39447f
marlesque <mqu000@citymail.cuny.edu>
2018-05-09 19:31:26 -0400

    Update state-popular-tweets.py

--- scripts/state-popular-tweets.py
+++ scripts/state-popular-tweets.py
@@ -1,27 +1,12 @@
 
-import io.archivesunleashed.spark.matchbox._
-import io.archivesunleashed.spark.matchbox.TweetUtils._
-import io.archivesunleashed.spark.rdd.RecordRDD._
+from pyspark import SparkConf, SparkContext
 
-tweets = spark.read.json(os.path.join(os.path.dirname(__file__)
-df = (
-    tweets
-    .withColumn("tokens", tokenize_udf(input_df['body']))
-    .withColumn("state", extract_state(input_df['gnip.profileLocations']))
-    .filter('state IS NOT NULL')
-    .filter(is_from_official_client(input_df['generator.displayName']))
-    .withColumn("user_id", remove_user_id_prefix(input_df['actor.id']))
-)
-df_tokens = df.select(df.id, df.user_id, F.explode(df.tokens).alias('word'))
-df_tokens.cache()
+conf= SparkConf().setMaster("local").setAppName("PopularTweetsByStates")
+tweetsDF = spark.read.json(os.path.join(os.path.dirname(__file__), '../data/cleaned.json'))
+tweetsDF.createOrReplaceTempView("tweets")
+hashTags_and_state= spark.sql("SELECT hashtags, state from tweets group by state")
+hashtags = hashTags_and_state.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
 
-df_topics_by_state = (
-    df_tokens
-    .join(df_topic_words, on='word')
-    .join(df_user_states, on='user_id')
-    .select('user_id', 'topic', 'state')
-    .groupby(['state', 'topic'])
-    .agg(F.countDistinct('user_id').alias('n_users'))
-)
-
-df_topics_by_state.cache()
+results = hashtags.collect()
+for result in results:
+    print(result)
